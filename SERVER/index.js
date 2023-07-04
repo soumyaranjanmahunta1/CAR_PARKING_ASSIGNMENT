d6f567d656e25faa7584fdb07641fafc468a9e38
soumyaranjanmahunta1 <happysoumya3@gmail.com>
2023-07-04 11:40:01 +0530

    before deploye

--- SERVER/index.js
+++ SERVER/index.js
@@ -10,9 +10,8 @@ app.use(bodyParser.json({ extend: true }));
 app.use(bodyParser.urlencoded({ extend: true }));
 app.use("/", router);
 const port = 8000;
-const UserName = process.env.db_userID;
-const Password = process.env.db_Password;
-connection(UserName, Password);
+
+connection();
 app.listen(port, () => {
   console.log(`Server is running successfully on port ${port}`);
 });
